f8f34ac5c3a06341ea4ea9f41b9e251cb69a35d2
ElMundodeLiam <angelcrystal1999@gmail.com>
2025-06-17 19:20:58 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ← ✅ Esto soluciona el problema de pantalla negra
   server: {
     port: 3000
   }
