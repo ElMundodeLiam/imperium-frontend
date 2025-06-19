5916e18663374423ba039979687b34b7bd5e4e78
ElMundodeLiam <angelcrystal1999@gmail.com>
2025-06-19 15:04:53 -0500

    Create AuthContext.jsx

---
+++ src/AuthContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState, useEffect } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [autenticado, setAutenticado] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setAutenticado(!!token);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ autenticado, setAutenticado }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
