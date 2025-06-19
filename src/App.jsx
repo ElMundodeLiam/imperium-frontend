74ddef3d53fd3561944bb7070b6b2576795d0be6
ElMundodeLiam <angelcrystal1999@gmail.com>
2025-06-19 14:54:45 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { useState, useEffect } from "react";
-
+import { useContext } from "react";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
@@ -8,48 +7,31 @@ import Tragamonedas from "./pages/Tragamonedas";
 import ApuestasFutbol from "./pages/ApuestasFutbol";
 import Ruleta from "./pages/Ruleta";
 import JuegosFuturos from "./pages/JuegosFuturos";
+import { AuthProvider, AuthContext } from "./AuthContext";
 
-function App() {
-  const [autenticado, setAutenticado] = useState(false);
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    setAutenticado(!!token);
-  }, []);
+function AppRoutes() {
+  const { autenticado } = useContext(AuthContext);
 
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={autenticado ? <Navigate to="/dashboard" /> : <Register />}
-        />
-        <Route
-          path="/login"
-          element={autenticado ? <Navigate to="/dashboard" /> : <Login />}
-        />
-        <Route
-          path="/dashboard"
-          element={autenticado ? <Dashboard /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/tragamonedas"
-          element={autenticado ? <Tragamonedas /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/apuestas-futbol"
-          element={autenticado ? <ApuestasFutbol /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/ruleta"
-          element={autenticado ? <Ruleta /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/juegos-futuros"
-          element={autenticado ? <JuegosFuturos /> : <Navigate to="/" />}
-        />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={autenticado ? <Navigate to="/dashboard" /> : <Register />} />
+      <Route path="/login" element={autenticado ? <Navigate to="/dashboard" /> : <Login />} />
+      <Route path="/dashboard" element={autenticado ? <Dashboard /> : <Navigate to="/" />} />
+      <Route path="/tragamonedas" element={autenticado ? <Tragamonedas /> : <Navigate to="/" />} />
+      <Route path="/apuestas-futbol" element={autenticado ? <ApuestasFutbol /> : <Navigate to="/" />} />
+      <Route path="/ruleta" element={autenticado ? <Ruleta /> : <Navigate to="/" />} />
+      <Route path="/juegos-futuros" element={autenticado ? <JuegosFuturos /> : <Navigate to="/" />} />
+    </Routes>
+  );
+}
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <AppRoutes />
+      </Router>
+    </AuthProvider>
   );
 }
 
