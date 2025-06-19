34e3ce5c52d5b9b730152cf8a2f3608e077028d4
ElMundodeLiam <angelcrystal1999@gmail.com>
2025-06-19 12:45:21 -0500

    Create ApuestasFutbol.jsx

---
+++ src/pages/ApuestasFutbol.jsx
@@ -0,0 +1,10 @@
+// src/pages/ApuestasFutbol.jsx
+import React from "react";
+
+export default function ApuestasFutbol() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-3xl font-bold">⚽ Apuestas de Fútbol - Próximamente...</h1>
+    </div>
+  );
+}
